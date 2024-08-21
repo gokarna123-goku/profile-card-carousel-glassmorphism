57a3482e246072a916518e54eb41db8ffd52ac51
Gokarna Adhikari <agokarna444@gmail.com>
2024-08-21 08:29:01 +0545

    modify the card border

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Card = ({ img, name, username, desc, link }) => {
     return (
-        <div className='w-full h-auto flex items-center justify-center flex-col space-y-6 rounded-xl p-6 backdrop-blur-md border border-neutral-200/60 bg-neutral-50/25'>
+        <div className='w-full h-auto flex items-center justify-center flex-col space-y-6 rounded-xl p-6 backdrop-blur-md border border-neutral-200/90 bg-neutral-50/25'>
             <img src={img} alt="" className="w-36 aspect-square object-cover object-center rounded-full p-0.5 shadow-lg" />
             <div className="space-y-2 text-center">
                 <div className="space-y-0.5">
