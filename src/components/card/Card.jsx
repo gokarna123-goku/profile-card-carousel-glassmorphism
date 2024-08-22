52e177b9a74f818be1d4061c90c1f89d536bf4ca
Gokarna Adhikari <agokarna444@gmail.com>
2024-08-22 20:49:33 +0545

    complete the bg img change

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 
 const Card = ({ img, name, username, desc, link }) => {
     return (
-        <div className='w-full h-auto flex items-center justify-center flex-col space-y-6 rounded-xl p-6 backdrop-blur-md border border-neutral-200/90 bg-neutral-50/25'>
+        <div className='w-full h-auto flex items-center justify-center flex-col space-y-6 rounded-xl p-6 backdrop-blur-md border border-neutral-200/10 shadow-lg bg-neutral-50/10'>
             <img src={img} alt="" className="w-36 aspect-square object-cover object-center rounded-full p-0.5 shadow-lg" />
             <div className="space-y-2 text-center">
                 <div className="space-y-0.5">
-                    <h1 className="text-2xl font-bold text-neutral-950">
+                    <h1 className="text-2xl font-bold text-neutral-200">
                         {name}
                     </h1>
-                    <p className="text-sm font-normal text-neutral-600">
+                    <p className="text-sm font-normal text-neutral-500">
                         @{username}
                     </p>
                 </div>
-                <p className="text-sm font-light text-neutral-700 line-clamp-3">
+                <p className="text-sm font-light text-neutral-400 line-clamp-3">
                     {desc}
                 </p>
             </div>
